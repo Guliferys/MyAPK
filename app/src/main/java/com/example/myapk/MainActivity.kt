9584be82cbe1f861f407f25d25b57ba1b623338c
Augustin Onta <onta.augustin@isa.utm.md>
2023-02-11 20:56:50 +0200

    First Start

---
+++ app/src/main/java/com/example/myapk/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.myapk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.myapk.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var bindingClass: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+
+    }
+}
